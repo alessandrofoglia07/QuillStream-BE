1af832645368a5f4d877a8d570eeebe240c835d6
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-06-19 22:04:42 +0200

    Add config file for environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,12 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            USER_POOL_ID: string;
-            POOL_CLIENT_ID: string;
+            COGNITO_USER_POOL: string;
+            COGNITO_USER_POOL_CLIENT: string;
+            WEBSOCKET_CONNECTIONS_TABLE: string;
+            DOCUMENTS_TABLE: string;
+            CORS_ORIGIN: string;
+            WEBSOCKET_API_ENDPOINT: string;
         }
     }
 }
