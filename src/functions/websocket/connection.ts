5540c0121370e21b45f3dece7980041aa8361a2a
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-07-16 17:34:52 +0200

    Add initial websocket connection and disconnection lambdas

--- src/functions/websocket/connection.ts
+++ src/functions/websocket/connection.ts
@@ -1,10 +1,105 @@
-import type { Handler } from 'aws-lambda';
+import type { APIGatewayProxyEvent, Handler } from 'aws-lambda';
+import { postToConnection } from '../../utils/postToConnection';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, GetCommand, PutCommand, PutCommandInput, UpdateCommand, UpdateCommandInput } from '@aws-sdk/lib-dynamodb';
+import { Document, UserDocument, WebSocketConnection } from '../../types';
+import { notifyClients } from '../../utils/notifyClients';
+import { formatDocument } from '../../utils/formatDocument';
 
-export const handler: Handler = async () => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'connection function executed successfully!'
-        })
-    };
+const client = new DynamoDBClient({ region: process.env.SERVERLESS_AWS_REGION });
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+export const handler: Handler = async (event: APIGatewayProxyEvent) => {
+    const { connectionId } = event.requestContext;
+    const { documentId } = event.queryStringParameters || {};
+    const userId = event.requestContext.authorizer!.jwt.claims.sub;
+    const username = event.requestContext.authorizer!.jwt.claims.username;
+
+    if (!connectionId) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: 'connectionId is required'
+            })
+        };
+    }
+
+    if (!documentId) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: 'documentId is required'
+            })
+        };
+    }
+
+    try {
+        const existingConnectionId = await ddbDocClient.send(new GetCommand({
+            TableName: process.env.WEBSOCKET_CONNECTIONS_TABLE,
+            Key: {
+                documentId, connectionId
+            }
+        }));
+
+        if (existingConnectionId.Item && await postToConnection((existingConnectionId.Item as WebSocketConnection).connectionId, JSON.stringify({ type: 'ping' }))) {
+            return {
+                statusCode: 403,
+                body: JSON.stringify({
+                    message: 'Connection already exists'
+                })
+            };
+        }
+
+        const params: PutCommandInput = {
+            TableName: process.env.WEBSOCKET_CONNECTIONS_TABLE,
+            Item: {
+                documentId,
+                connectionId,
+                userId,
+                username
+            }
+        };
+
+        await ddbDocClient.send(new PutCommand(params));
+
+        notifyClients(documentId, JSON.stringify({ type: 'connection', message: `User ${username} connected.` }));
+
+        const updateParams: UpdateCommandInput = {
+            TableName: process.env.USER_DOCUMENTS_TABLE,
+            Key: {
+                userId, documentId
+            },
+            UpdateExpression: 'SET lastAccessedAt = :lastAccessedAt',
+            ExpressionAttributeValues: {
+                ':lastAccessedAt': Date.now().toString()
+            },
+            ReturnValues: 'ALL_NEW'
+        };
+
+        const newUserDocument = (await ddbDocClient.send(new UpdateCommand(updateParams))).Attributes;
+
+        const document = await ddbDocClient.send(new GetCommand({
+            TableName: process.env.DOCUMENTS_TABLE,
+            Key: {
+                documentId
+            }
+        }));
+
+        const fullDoc = formatDocument(document.Item as Document, newUserDocument as UserDocument);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'Connection established',
+                document: fullDoc
+            })
+        };
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'Internal server error'
+            })
+        };
+    }
 };
--- src/functions/websocket/disconnection.ts
+++ src/functions/websocket/disconnection.ts
@@ -1,10 +1,69 @@
-import type { Handler } from 'aws-lambda';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DeleteCommand, DeleteCommandInput, DynamoDBDocumentClient, QueryCommand, QueryCommandInput } from '@aws-sdk/lib-dynamodb';
+import type { APIGatewayProxyEvent, Handler } from 'aws-lambda';
+import type { WebSocketConnection } from '../../types';
+import { notifyClients } from '../../utils/notifyClients';
 
-export const handler: Handler = async () => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'disconnection function executed successfully!'
-        })
+const client = new DynamoDBClient({ region: process.env.SERVERLESS_AWS_REGION });
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+export const handler: Handler = async (event: APIGatewayProxyEvent) => {
+
+    const { connectionId } = event.requestContext;
+
+    if (!connectionId) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: 'connectionId is required'
+            })
+        };
+    }
+
+    const getParams: QueryCommandInput = {
+        TableName: process.env.WEBSOCKET_CONNECTIONS_TABLE,
+        IndexName: 'connectionId-index',
+        KeyConditionExpression: 'connectionId = :connectionId',
+        ExpressionAttributeValues: {
+            ':connectionId': connectionId
+        }
     };
+
+    try {
+        const { Items } = await ddbDocClient.send(new QueryCommand(getParams));
+        if (!Items || Items.length === 0) {
+            return {
+                statusCode: 404,
+                body: JSON.stringify({
+                    message: 'Connection not found'
+                })
+            };
+        }
+
+        const deleteParams: DeleteCommandInput = {
+            TableName: process.env.WEBSOCKET_CONNECTIONS_TABLE,
+            Key: {
+                documentId: (Items[0] as WebSocketConnection).documentId,
+                connectionId
+            }
+        };
+
+        await ddbDocClient.send(new DeleteCommand(deleteParams));
+
+        notifyClients((Items[0] as WebSocketConnection).documentId, JSON.stringify({ type: 'disconnection', message: `User ${(Items[0] as WebSocketConnection).username} disconnected.` }));
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'Connection deleted'
+            })
+        };
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'Internal server error'
+            })
+        };
+    }
 };
